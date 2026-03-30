d6bcc686c4b180d69a48f823f964cf8d87882ccf
joshualagat <80578895+joshualagat@users.noreply.github.com>
2026-03-30 22:59:39 +0800

    Updated page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 import Leaderboard from '../components/Leaderboard';
 
+// Next.js Route Segment Config: Revalidate page every 5 seconds!
+export const revalidate = 5;
+
 export default function Home() {
   return (
     <main>
